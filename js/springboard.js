3547d85e4db4706e529974368c4d7b906fcc49f5
Aanand Kainth <akainth015@gmail.com>
2019-06-29 16:31:33 -0700

    Scaffold project

---
+++ js/springboard.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCNnu5sEkT_VzqC0GTPT7tv_IziF-Azw2I",
+    authDomain: "springboard-core.firebaseapp.com",
+    databaseURL: "https://springboard-core.firebaseio.com",
+    projectId: "springboard-core",
+    storageBucket: "",
+    messagingSenderId: "292547213939",
+    appId: "1:292547213939:web:be832e17c74dd097"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
