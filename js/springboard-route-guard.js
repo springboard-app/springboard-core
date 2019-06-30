57fc0af9767e37276dcb260d85a4935964558cc8
Aanand Kainth <akainth015@gmail.com>
2019-06-29 17:41:13 -0700

    Create projects functionality

--- js/springboard-route-guard.js
+++ js/springboard-route-guard.js
@@ -3,3 +3,11 @@ firebase.auth().onAuthStateChanged(user => {
         location.replace("index.html");
     }
 });
+
+firebase.auth.authSignIn = new Promise((resolve, reject) => {
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            resolve(user);
+        }
+    })
+});
